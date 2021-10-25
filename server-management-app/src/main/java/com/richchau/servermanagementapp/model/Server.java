47b6dc529e1aae853252cbe0ec44da66c9a59055
Rich Chau <richsensations@gmail.com>
2021-10-25 14:01:55 -0700

    Added Status enum and annotations for sql mapping

--- server-management-app/src/main/java/com/richchau/servermanagementapp/model/Server.java
+++ server-management-app/src/main/java/com/richchau/servermanagementapp/model/Server.java
@@ -1,11 +1,33 @@
 package com.richchau.servermanagementapp.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.AUTO;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import com.richchau.servermanagementapp.enumeration.Status;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
     private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Address cannot be empty or null")
     private String ipAddress;
     private String name;
     private String memory;
     private String type;
     private String imageUrl;
-    // private Status status;
+    private Status status;
 }
