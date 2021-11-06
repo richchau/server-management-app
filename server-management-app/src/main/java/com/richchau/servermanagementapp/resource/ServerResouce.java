15dd69c538da0237a2e6cbd9fdbd8e7e16146cdc
Rich Chau <richsensations@gmail.com>
2021-11-06 07:57:14 -0700

    Added ping mapping to server resource

--- server-management-app/src/main/java/com/richchau/servermanagementapp/resource/ServerResouce.java
+++ server-management-app/src/main/java/com/richchau/servermanagementapp/resource/ServerResouce.java
@@ -1,12 +1,16 @@
 package com.richchau.servermanagementapp.resource;
 
+import java.io.IOException;
 import java.util.Map;
 
+import com.richchau.servermanagementapp.enumeration.Status;
 import com.richchau.servermanagementapp.model.Response;
+import com.richchau.servermanagementapp.model.Server;
 import com.richchau.servermanagementapp.service.implementation.ServerServiceImpl;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,13 @@ public class ServerResouce {
     public ResponseEntity<Response> getServers() {
         return ResponseEntity.ok(Response.builder().timeStamp(now()).data(Map.of("servers", serverService.list(30)))
                 .message("Servers retrieved").status(OK).statusCode(OK.value()).build());
+    }
+
+    @GetMapping("/ping/{ipAddress}")
+    public ResponseEntity<Response> pingServers(@PathVariable("ipAddress") String ipAddress) throws IOException {
+        Server server = serverService.ping(ipAddress);
+        return ResponseEntity.ok(Response.builder().timeStamp(now()).data(Map.of("server", server))
+                .message(server.getStatus() == Status.SERVER_UP ? "Ping success" : "Ping failed").status(OK)
+                .statusCode(OK.value()).build());
     }
 }
